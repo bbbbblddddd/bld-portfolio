cd18681bf774e6e9d9278e00cb2e2005bac3911c
Brian Dickson <brianlawrencedickson@gmail.com>
2023-01-16 09:46:36 +0000

    image showing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,11 @@
 import './Home.css'
-import image from "/public/images/brian.png";
+import image from "../images/brian.png";
 
 
 const Home = () => {
 
     return (
         <>
-        <h1>Welcome</h1>
         <img src= {image} alt=""/>
         </>
 
